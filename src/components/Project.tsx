c0bd711e731b02a275d5664cb6a1fdd99f7106ec
Thomas Fogarty <t.fogarty24@outlook.com>
2024-11-18 20:31:37 +0000

    webhook test

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,7 +3,7 @@ const Project: React.FC<{ title: string; description: string, url:string }> = (p
     
       <div className="mb-6 rounded">
         <h2 className="text-xl font-semibold text-gray-400 mb-1">
-          <a href={props.url}>{props.title}</a>
+          <a href={props.url}>{props.title} app</a>
         </h2>
         <div>
           <p className="text-gray-200 leading-relaxed">{props.description}</p>
