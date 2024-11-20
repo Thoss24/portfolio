93def55c2e635cda9cc7f12f2bb884b71de70f11
Thomas Fogarty <t.fogarty24@outlook.com>
2024-11-20 22:01:43 +0000

    project title changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import About from "./components/About";
 import Heading from "./components/Heading";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    document.title = "My Portfolio";
+  }, [])
 
   return (
     <div className="bg-zinc-700 pl-14 pr-14 pb-10 flex flex-col">
